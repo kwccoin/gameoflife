e5d6a795d8a194d1d6edd0a360f340d0bfc36704
Kowloon walled city (coin) <kwccoin@users.noreply.github.com>
2021-12-04 12:24:53 +0800

    more info

--- ycanvas.js
+++ ycanvas.js
@@ -1,6 +1,19 @@
+// live console 
+
+// may need to copy the 
+// "live server local host address" to chrome
+
+// chrome docker side on ... 
+
+// from yt: https://www.youtube.com/watch?v=83L6B13ixQ0&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL&index=3
+//  Chris Course Drawing On the Canvas | HTML5 Canvas Tutorial for Beginners - Ep. 2
+
+
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = canvas.getContext('2d');
 
 ctx.fillRect(10,10,100,100);
+
+console.log(canvas)
