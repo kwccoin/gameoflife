0f53e6ae1c6d55de0061e565cfab3437c7d311a5
Kowloon walled city (coin) <kwccoin@users.noreply.github.com>
2021-12-04 12:20:05 +0800

    trying canvas using live server

---
+++ ycanvas.js
@@ -0,0 +1,6 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext('2d');
+
+ctx.fillRect(10,10,100,100);
